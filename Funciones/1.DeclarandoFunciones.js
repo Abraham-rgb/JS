f2c9d5976587aee14fe482cddc02d855c888f38f
Abraham <abrahamslp1991@gmail.com>
2021-01-09 21:54:50 -0500

    practice Function

---
+++ Funciones/1.DeclarandoFunciones.js
@@ -0,0 +1,17 @@
+function mostrarMensaje () {
+    console.log('Hola mundo')
+};
+
+// mostrarMensaje()
+
+let miVariable = 5;
+
+function multiplicador (){
+    miVariable *= 2
+};
+
+for (let i = 0; i < 10; i++){
+    multiplicador();
+}
+console.log(miVariable);
+
