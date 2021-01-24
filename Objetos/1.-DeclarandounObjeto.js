afa7f2965f01b34466cb04903c35d3e64423c982
Abraham <abrahamslp1991@gmail.com>
2021-01-24 13:33:35 -0500

    Objets

---
+++ Objetos/1.-DeclarandounObjeto.js
@@ -0,0 +1,17 @@
+let empleado = {
+    nombre:'Juan',
+    edad: 30,
+    'Talla camisa': 'M',
+    equipo:{
+        SO:'windows',
+        marca:'Legion'
+    }
+};
+
+// console.log(empleado);
+// console.log(`Nombre: ${empleado.nombre}\r\nEdad: ${empleado.edad}`)
+
+let empleado2 = 'nombre';
+// console.log(empleado[empleado2])
+// console.log(empleado['Talla camisa'])
+// console.log(empleado.equipo.SO)
